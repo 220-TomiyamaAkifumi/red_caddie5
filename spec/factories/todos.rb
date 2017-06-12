a6910adc25c7a2201eaa9ce105347500c74c1189
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2017-06-13 01:00:51 +0900

    [wip] add locale

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     user_id 1
     title "MyString"
     note "MyText"
-    due_date "2017-06-12"
-    completed_on "2017-06-12"
+    due_date "2017-06-13"
+    completed_on "2017-06-13"
     repeat 1
     urgent false
   end
